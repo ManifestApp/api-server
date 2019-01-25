4b9c5174d774b1adaff5193390e597854a41bf58
Matthieu Bulté <matthieu@brili.com>
2019-01-25 21:23:40 +0100

    Add minimal documentation of the routes

--- src/app.js
+++ src/app.js
@@ -16,24 +16,44 @@ databaseAccess.initialize();
 
 // Setup routes
 // Protest
+
+// body: { upperLeft, lowerRight }
 app.post("/protest/search", protestController.findByLocation);
+
+// body: { title, startingPosition, startingTime, ?description }
 app.post("/protest", auth, protestController.create);
+
+// body: { id, creator, title, startingPosition, startingTime, ?description }
 app.put("/protest", auth, protestController.update);
+
 app.delete("/protest/:protestId", auth, protestController.delete);
-app.get("/protester/protests", auth, protestController.findByCreator);
+
+app.get("/protest/my", auth, protestController.findByCreator);
 
 // Protester
+
+// body: { deviceToken, ?nickname }
 app.post("/protester", protesterController.create);
+
+// body: { deviceToken, ?nickname }
 app.put("/protester", auth, protesterController.update);
+
 app.delete("/protester", auth, protesterController.delete);
+
 app.post("/protester/join/:protestId", auth, protesterController.joinProtest);
 
 // Trust relation
-app.get("/trust", auth, trustRelationController.findByProtester);
-app.post("/trust", auth, trustRelationController.create);
-app.delete("/trust/:relationId", auth, trustRelationController.delete);
-app.put("/trust/:relationId/fulfill", auth, trustRelationController.fulfill);
-app.put("/trust/:relationId/expirationDate", auth, trustRelationController.updateRelationDate);
 
+app.get("/trust/my", auth, trustRelationController.findByProtester);
+
+// body: { ?expirationDate }
+app.post("/trust", auth, trustRelationController.create);
+
+app.delete("/trust/:relationId", auth, trustRelationController.delete);
+
+app.put("/trust/:relationId/fulfill", auth, trustRelationController.fulfill);
+
+// body: { expirationDate }
+app.put("/trust/:relationId/expirationDate", auth, trustRelationController.updateRelationDate);
 
 module.exports = app;
